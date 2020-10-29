c1ab9efc1a3047cded8e29d6547f15e5b0d0cd80
NivRichter <nivrichter@gmail.com>
2020-10-29 22:31:42 +0200

    search engine added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import "bootstrap/dist/css/bootstrap.css";
 // import 'react-day-picker/lib/style.css';
 import './App.css';
 
+import ReactGA from 'react-ga';
+import Fuse from 'fuse.js';
+
+
+
 import {
   Button,
   Container,
@@ -34,6 +39,13 @@ class App extends Component {
     super(props);
     this.handleDayClick = this.handleDayClick.bind(this);
 
+    const trackingId = "UA-180348020-1"; 
+    ReactGA.initialize(trackingId, {
+      debug: true
+    });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
+
 
 
  
@@ -51,6 +63,9 @@ class App extends Component {
       Jerusalem: true,
       center:true,
       first:true,
+      suggestions:[],
+      chosen_park: "",
+      input:"",
       sites_by_id : {
          "13":{
             "name":"שמורת טבע נחל עיון",
@@ -365,11 +380,35 @@ class App extends Component {
       }
     };
 
-   
+  }
+  componentDidMount(){
+   this.initData();
+   this.getAllSitesName()
   }
 
+   initData(){
+     console.log("init data")
+   let url = `https://checkfrontcom.checkfront.com/api/3.0/item`
+   fetch(url, {
+     method: "GET", // *GET, POST, PUT, DELETE, etc.
+     mode: "cors", // no-cors, *cors, same-origin
+     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+     credentials: "same-origin", // include, *same-origin, omit
+   })
+     .then((res) => res.json())
+     .then((data) =>this.setState({ picturesDB: data.items },function(){
+        this.getAllSitesName()
+         } ))
+ }
 
-
+getAllSitesName(){
+   const sitesData=this.state.picturesDB;
+   let names =[]
+   for (let item_id in sitesData){
+      names = names.concat([sitesData[item_id]["name"]])
+   }
+   this.setState({suggestions: names})
+}
   handleDayClick(day) {
     this.setState({ selectedDay: day, view:false,  
        north: true,
@@ -383,25 +422,26 @@ class App extends Component {
 
   }
 
-  getPicturesData(){
-   const selectedDay = this.state.formatDate
-   let url = `https://checkfrontcom.checkfront.com/api/3.0/item?item_id=11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,3,30,32,33,34,37,38,39,4,40,44,45,48,49,50,51,52,53,54,55,56,57,59,6,60,61,62,64,65,66,67,69,7,76,77,8,80,81,82,9,90,93,94&start_date=${selectedDay}&end_date=${selectedDay}`;
-   fetch(url, {
-     method: "GET", // *GET, POST, PUT, DELETE, etc.
-     mode: "cors", // no-cors, *cors, same-origin
-     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-     credentials: "same-origin", // include, *same-origin, omit
-   })
-     .then((res) => res.json())
-     .then((data) =>this.setState({ picturesDB: data.items }) )
-}
+//   async getPicturesData(){
+//    const selectedDay = this.state.formatDate
+//    //let url = `https://checkfrontcom.checkfront.com/api/3.0/item?item_id=11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,3,30,32,33,34,37,38,39,4,40,44,45,48,49,50,51,52,53,54,55,56,57,59,6,60,61,62,64,65,66,67,69,7,76,77,8,80,81,82,9,90,93,94&start_date=${selectedDay}&end_date=${selectedDay}`;
+//    let url = `https://checkfrontcom.checkfront.com/api/3.0/item?istart_date=${selectedDay}&end_date=${selectedDay}`
+//    fetch(url, {
+//      method: "GET", // *GET, POST, PUT, DELETE, etc.
+//      mode: "cors", // no-cors, *cors, same-origin
+//      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//      credentials: "same-origin", // include, *same-origin, omit
+//    })
+//      .then((res) => res.json())
+//      .then((data) =>this.setState({ picturesDB: data.items }) )
+// }
 
-  getData() {
-   this.getPicturesData()
-    console.log("mounting APP");
+  async getData() {
+   //await this.getPicturesData()
     const selectedDay = this.state.formatDate
-    let url = `https://checkfrontcom.checkfront.com/api/3.0/item/cal?item_id=11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,3,30,32,33,34,37,38,39,4,40,44,45,48,49,50,51,52,53,54,55,56,57,59,6,60,61,62,64,65,66,67,69,7,76,77,8,80,81,82,9,90,93,94&start_date=${selectedDay}&end_date=${selectedDay}`;
+    //let url = `https://checkfrontcom.checkfront.com/api/3.0/item/cal?item_id=11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,3,30,32,33,34,37,38,39,4,40,44,45,48,49,50,51,52,53,54,55,56,57,59,6,60,61,62,64,65,66,67,69,7,76,77,8,80,81,82,9,90,93,94&start_date=${selectedDay}&end_date=${selectedDay}`;
     //let url = "https://checkfrontcom.checkfront.com/api/3.0/item/cal?item_id=40,14,77&start_date=20201024&end_date=20201025"
+    let url = `https://checkfrontcom.checkfront.com/api/3.0/item/cal?start_date%20=${selectedDay}&end_date=${selectedDay}`
     fetch(url, {
       method: "GET", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
@@ -420,15 +460,53 @@ class App extends Component {
     const sites = this.state.avilableSites;
   }
 
+
+
+convertRegionID2Name(id){
+   switch(id) {
+      case 56:
+         return "north"
+      case 57:
+         return "center"
+      case 58:
+         return "Jerusalem";
+      case 59:
+         return "south";
+      default:
+         return "center"
+    }
+}
+
 getAvailabeSites(){
-  const data = this.state.jsonData["items"];
-  const ids = [11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,3,30,32,33,34,37,38,39,4,40,44,45,48,49,50,51,52,53,54,55,56,57,59,6,60,61,62,64,65,66,67,69,7,76,77,8,80,81,82,9,90,93,94]
-  const date = this.state.formatDate
-  // const undefs = ids.filter((id)=>data[id] === undefined)
-  // console.log(undefs)
-  return ids.reduce( (acc,curr)=> data[curr][date]===1 ? {...acc, [curr]:this.state.sites_by_id[curr] }: acc ,{})
+
+   const sitesAvailablity = this.state.jsonData["items"];
+   //const ids = [11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,3,30,32,33,34,37,38,39,4,40,44,45,48,49,50,51,52,53,54,55,56,57,59,6,60,61,62,64,65,66,67,69,7,76,77,8,80,81,82,9,90,93,94]
+   const date = this.state.formatDate
+   const sitesData=this.state.picturesDB;
+   let avilableSites ={}
+   let repeated_sites_to_ignore = []
+   for (let item_id in sitesAvailablity){
+      if ( !repeated_sites_to_ignore.includes(item_id) ){
+         if(sitesAvailablity[item_id][date] === 1 && item_id in this.state.sites_by_id ) {
+            if( sitesData[item_id]["product_group_children"].length > 0){
+               let to_ignore = sitesData[item_id]["product_group_children"]
+               //console.log(`to ignore: ${typeof(to_ignore)}`)
+               repeated_sites_to_ignore = repeated_sites_to_ignore.concat(to_ignore.reduce( (acc,curr) => acc.concat([curr["item_id"]]),[]))
+            }
+            avilableSites[item_id] = {
+               name:sitesData[item_id]["name"],
+               url:this.state.sites_by_id[item_id]?.["url"],
+               region:this.convertRegionID2Name(sitesData[item_id]["category_id"])
+            }
+         }
+
+      }
+
+   }
+  return avilableSites;
 
 }
+
  formatDate(date) {
   var d = new Date(date),
       month = '' + (d.getMonth() + 1),
@@ -455,6 +533,12 @@ formatIsraeliDate(date) {
   return [day,month,year].join('/');
 }
 
+
+
+handleChange = (event) => {
+   this.setState({input: event.target.value});
+ }
+
   render() {
  
     let avilableSites = this.state.avilableSites;
@@ -463,10 +547,11 @@ formatIsraeliDate(date) {
     let checkedJ = this.state.Jerusalem;
     let checkedC = this.state.center;
     let checkedS = this.state.south;
+    let parks_names = [];
     const today = new Date();
-    const fortnightAway = new Date(Date.now() + 12096e5);
+    const fortnightAway = new Date(Date.now() + 12096e5); // set a calnder disabler for 2 weeks away
 
-
+   let input = this.state.input
 
 
 
@@ -476,17 +561,29 @@ formatIsraeliDate(date) {
          let url = avilableSites[id]["url"]
          let region = this.state.avilableSites[id]["region"]
          if (this.state[region]){
-            let background = this.state.picturesDB[id]?.image?.[1]?.url || this.state.picturesDB[id]?.image?.[2]?.url || this.state.picturesDB[id]?.image?.[3]?.url 
+            let background = this.state.picturesDB[id]?.image?.[1]?.url || this.state.picturesDB[id]?.image?.[2]?.url || this.state.picturesDB[id]?.image?.[3]?.url ; 
+            parks_names.push(name);
             sitesTable[region].push (
-               <div className="card w3-blue" key={id} style={{ 
-                  backgroundImage:"url("+background+")"
-               }} > <a className ="hyperLink" href={url}>{name} </a></div>
-        )
+              {  name:name,
+                 element: 
+                  <a href={url}>
+                        <div className="card w3-blue" key={id} 
+                        style={{  backgroundImage:"url("+background+")"  }} >
+                        <a className ="hyperLink" >{name} </a>
+                        </div>
+                     </a>
+                  }
+              
+          )
+         }
       }
+   }
 
-      }
-    
-    }
+   const fuseNorth = new Fuse(sitesTable["north"], {keys: ['name'], threshold:0.5})
+   const fuseCenter = new Fuse(sitesTable["center"], {keys: ['name'], threshold:0.5})
+   const fuseJerusalem = new Fuse(sitesTable["Jerusalem"], {keys: ['name'], threshold:0.5})
+   const fuseSouth = new Fuse(sitesTable["south"], {keys: ['name'], threshold:0.5})
+
 
     return (
 
@@ -495,15 +592,15 @@ formatIsraeliDate(date) {
          <Container fluid className="text-light bg-teva-green py-3">
            <Row>
               <Col>
-                  <h1>לאן נטייל מחר? 🏕️</h1>
-                  <h2>זמינות שמורות טבע ופארקים לאומיים</h2>
+              <h1>לאן נטייל מחר? 🏕️ <a class="text-light" href="http://ganleumi.online">ganleumi.online</a></h1>
+              <h2>זמינות שמורות טבע ופארקים לאומיים</h2>
               </Col>
            </Row>
         </Container>
 
          <Navbar className="bg-teva-lime">
             <Nav className="mx-auto justify-content-center">
-               <Nav.Link active href="">חיפוש תאריך</Nav.Link>
+               <Nav.Link active href="">  חיפוש תאריך </Nav.Link>
                <Nav.Link disabled href="">תצוגת טבלה (בקרוב!)</Nav.Link>
                {/* <Button variant="outline-info" onClick={randomClicked}>הגרל אתר שפנוי היום</Button> */}
             </Nav>
@@ -520,7 +617,6 @@ formatIsraeliDate(date) {
                         localeUtils={MomentLocaleUtils} 
                         locale={this.state.locale}/>
                      {this.state.selectedDay ? 
-                        //<p>You clicked {this.state.selectedDay.toLocaleDateString()}</p>
                         <div className="alert alert-warning">
                         <strong className="d-blockalert">מציג אתרים הזמינים בתאריך:  {this.formatIsraeliDate(this.state.selectedDay)}</strong>
                         </div>
@@ -530,6 +626,8 @@ formatIsraeliDate(date) {
                   </div>
                </Col>
             </Row>
+
+
          </Container>
 
 
@@ -616,6 +714,13 @@ formatIsraeliDate(date) {
                                                 <a className="region"> דרום</a>
                                           </ToggleButton>
                                        </ButtonGroup>
+                                                                  
+                                       <Row>
+                                          <Col>
+                                                   חיפוש:
+                                                <input type="text" value={this.state.input} onChange={this.handleChange} placeholder="הכנס שם פארק לאומי"  />
+                                          </Col>
+                                       </Row>
                                     </div>
                                  : <div></div>
                               }
@@ -639,28 +744,39 @@ formatIsraeliDate(date) {
                                              <h3>צפון</h3>
                                           </div>
                                           <section className="basic-grid">
-                                             {sitesTable["north"]}
+                                             {
+                                                input.length=== 0 ?  sitesTable.north.map(item => item.element) :
+                                                fuseNorth.search(this.state.input).map(x => x.item.element)
+                                             }
                                           </section>
 
                                           <div className="region bg-teva-lime mt-5 mb-2 py-2">
                                              <h3>מרכז</h3>
                                           </div>
                                           <section className="basic-grid">
-                                             {sitesTable["center"]}
-                                          </section>
+                                             {
+                                                input.length=== 0 ?  sitesTable.center.map(item => item.element) :
+                                                fuseCenter.search(this.state.input).map(x => x.item.element)
+                                             }                                          </section>
 
                                           <div className="region bg-teva-lime mt-5 mb-2 py-2">
                                              <h3>ירושלים</h3>
                                           </div>
                                           <section className="basic-grid">
-                                             {sitesTable["Jerusalem"]}
+                                            {
+                                                input.length=== 0 ?  sitesTable.Jerusalem.map(item => item.element) :
+                                                fuseJerusalem.search(this.state.input).map(x => x.item.element)
+                                             }                                      
                                           </section>
 
                                           <div className="region bg-teva-lime mt-5 mb-2 py-2">
                                              <h3>דרום</h3>
                                           </div>
                                           <section className="basic-grid">
-                                             {sitesTable["south"]}
+                                             {
+                                                input.length=== 0 ?  sitesTable.south.map(item => item.element) :
+                                                fuseSouth.search(this.state.input).map(x => x.item.element)
+                                             }                                            
                                           </section>
                                        </div>
 
@@ -691,6 +807,9 @@ formatIsraeliDate(date) {
                      <div className="my-2">
                         <span>👨‍💻 הפרוייקט בקוד פתוח! מוזמנות ומוזמנים להציע הארות והערות </span>
                         <a href="https://github.com/NivRichter/park_n_recs" target="_blank">בעמוד הגיטהאב של הפרוייקט</a>
+                     </div>
+                     <div className="my-2">
+                        <span>🚮 טיול נעים! ובבקשה אל תשאירו אשפה בטבע </span>
                      </div>
                   </Col>
                </Row>
